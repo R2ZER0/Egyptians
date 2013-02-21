9b57b6de119e270aa6a827328692d3e2feee9584
Rikki Guy <rikki.guy@gmx.com>
2013-02-21 23:36:47 +0000

    added abstract viewable entity

---
+++ src/egyptians/ViewableEntity.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package egyptians;
+
+import org.lwjgl.util.vector.Vector2f;
+import org.newdawn.slick.Image;
+
+/**
+ *
+ * @author rikki
+ */
+abstract public class ViewableEntity {
+    
+    public Image entityImage = null;
+    
+    public Vector2f pos, size;
+    
+    public ViewableEntity(Image img, Vector2f pos, Vector2f size)
+    {
+        this.entityImage = img;
+        this.pos = pos;
+        this.size = size;
+    }    
+}
