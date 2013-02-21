98e128aa9aad9975bcacebf4812e25dbbb4383e7
Rikki Guy <rikki.guy@gmx.com>
2013-02-21 20:06:32 +0000

    Create slick BasicGame based outline

--- src/meteor/Meteor.java
+++ src/meteor/Meteor.java
@@ -4,16 +4,45 @@
  */
 package meteor;
 
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
 /**
  *
- * @author rikki
+ * @author rikki & lewis
  */
-public class Meteor {
+public class Meteor extends BasicGame {
+    
+    private Meteor()
+    {
+        super("Meteor");
+    }
+    
+    @Override public void init(GameContainer gc) throws SlickException
+    {
+        //init
+    }
+    
+    @Override public void update(GameContainer gc, int delta) throws SlickException
+    {
+        //update
+    }
+    
+    @Override public void render(GameContainer gc, Graphics g) throws SlickException
+    {
+        //render
+    }
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws SlickException
+    {
+        AppGameContainer app = new AppGameContainer(new Meteor());
+        app.setDisplayMode(1024, 600, false);
+        app.start();
     }
 }
