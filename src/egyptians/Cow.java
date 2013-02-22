3381fb5bb3f1241c6e0507f2c994cc4c3e9e9a83
Rikki Guy <rikki.guy@gmx.com>
2013-02-22 04:37:23 +0000

    Don't excessively make images

--- src/egyptians/Cow.java
+++ src/egyptians/Cow.java
@@ -4,6 +4,8 @@
  */
 package egyptians;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Vector2f;
@@ -14,7 +16,8 @@ import org.newdawn.slick.geom.Vector2f;
  */
 public class Cow extends Entity {
     
-    public final static Vector2f COW_SIZE = new Vector2f(195, 99);
+    public static Image cowImage = null;    
+    public final static Vector2f COW_SIZE = new Vector2f(180, 99);
     public final static double COW_GRAVITY = 0.6;
 
     @Override public void think(int delta)
@@ -24,10 +27,22 @@ public class Cow extends Entity {
         pos.y += COW_GRAVITY * delta;
     }
     
+    static
+    {
+        if(cowImage == null)
+        {
+            try {
+                cowImage = new Image("cow.png");
+            } catch (SlickException ex) {
+                Logger.getLogger(Cow.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
     public Cow(float posx) throws SlickException
     {
         // FIXME - don't create a new image every time (same for dude)
-        super(new Image("cow.png"), new Vector2f(posx, -100), COW_SIZE);
+        super(cowImage, new Vector2f(posx, -100), COW_SIZE);
     }
     
 }
--- src/egyptians/Dude.java
+++ src/egyptians/Dude.java
@@ -4,6 +4,8 @@
  */
 package egyptians;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Vector2f;
@@ -14,12 +16,25 @@ import org.newdawn.slick.geom.Vector2f;
  */
 public class Dude extends Entity {
     
+    public static Image dudeImage = null;
     public final static Vector2f DUDE_SIZE = new Vector2f(66, 96);
     public final static float DUDE_SPEED = 0.2f;
     
+    static
+    {
+        if(dudeImage == null)
+        {
+            try {
+                dudeImage = new Image("dude.png");
+            } catch (SlickException ex) {
+                Logger.getLogger(Dude.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
     public Dude(Vector2f pos) throws SlickException
     {
-        super(new Image("dude.png"), pos, DUDE_SIZE);
+        super(dudeImage, pos, DUDE_SIZE);
     }
     
     @Override public void think(int delta)
